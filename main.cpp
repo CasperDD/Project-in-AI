43a267e134c3e97889aec27daf96ed81ba9efce7
Dyxore <casperdamhus@live.dk>
2022-12-05 17:51:32 +0100

    created repository

---
+++ main.cpp
@@ -0,0 +1,132 @@
+// include simulation environment stuff
+#include <ode_robots/simulation.h>
+// include agent (class for holding a robot, a controller and a wiring)
+#include <ode_robots/odeagent.h>
+// playground
+#include <ode_robots/playground.h>
+// simple wiring
+#include <selforg/one2onewiring.h>
+// the robot
+#include <ode_robots/hexabot.h>
+// the controller
+#include "MyController.h"
+// joint needed for fixation of the robot in the beginning
+#include <ode_robots/joint.h>
+
+class ThisSim : public lpzrobots::Simulation
+{
+public:
+  ThisSim()
+  {
+    addPaletteFile("colors/UrbanExtraColors.gpl");
+    addColorAliasFile("colors/UrbanColorSchema.txt");
+    // you can replace color mappings in your own file, see colors/UrbanColorSchema.txt
+    // addColorAliasFile("myColorSchema.txt");
+    setGroundTexture("Images/whiteground.jpg"); // gets its color from the schema
+    // setTitle("centered text");
+    // setCaption("right aligned text");
+  }
+
+  /**
+   * starting function (executed once at the beginning of the simulation loop)
+   */
+  virtual void start(const lpzrobots::OdeHandle &odeHandle,
+                     const lpzrobots::OsgHandle &osgHandle,
+                     lpzrobots::GlobalData &global)
+  {
+    // set initial camera position
+    setCameraHomePos(
+        lpzrobots::Pos(-0.0114359, 6.66848, 0.922832),
+        lpzrobots::Pos(178.866, -7.43884, 0));
+
+    // set simulation parameters
+    global.odeConfig.setParam("controlinterval", 30);
+    global.odeConfig.setParam("simstepsize", 0.001);
+
+    // add playground
+    lpzrobots::Playground *playground = new lpzrobots::Playground(odeHandle, osgHandle,
+                                                                  osg::Vec3(10, 0.2, 0.3));
+    // playground->setTexture(0, 0, lpzrobots::TextureDescr("Images/wall_bw.jpg", -1.5, -3));
+    // playground->setPosition(osg::Vec3(0, 0, .0));
+    // global.obstacles.push_back(playground);
+
+    // Add Hexabot robot
+    HEXABOT::HexabotConf hexabotConf = lpzrobots::Hexabot::getDefaultConf();
+    lpzrobots::OdeHandle rodeHandle = odeHandle;
+    rodeHandle.substance = lpzrobots::Substance(3.0, 0.0, 50.0, 0.8);
+    hexabot = new lpzrobots::Hexabot(
+        rodeHandle,
+        osgHandle.changeColor(lpzrobots::Color(1, 1, 1)),
+        hexabotConf, "Hexabot");
+
+    // put hexabot a little bit in the air
+    hexabot->place(osg::Matrix::translate(.0, .0, 1.));
+
+    controller = new MyController();
+    // create wiring
+    One2OneWiring *wiring = new One2OneWiring(new ColorUniformNoise());
+
+    // create agent and init it with controller, robot and wiring
+    lpzrobots::OdeAgent *agent = new lpzrobots::OdeAgent(global);
+    agent->init(controller, hexabot, wiring);
+
+    // create a fixed joint to hold the robot in the air at the beginning
+    robotfixator = new lpzrobots::FixedJoint(
+        hexabot->getMainPrimitive(),
+        global.environment);
+    robotfixator->init(odeHandle, osgHandle, false);
+
+    // inform global variable over everything that happened:
+    global.configs.push_back(hexabot);
+    global.agents.push_back(agent);
+    global.configs.push_back(controller);
+
+    std::cout << "\n\n"
+              << "################################\n"
+              << "#   Press x to free hexabot!    #\n"
+              << "################################\n"
+              << "\n\n"
+              << std::endl;
+  }
+
+  /**
+   * add own key handling stuff here, just insert some case values
+   */
+  virtual bool command(const lpzrobots::OdeHandle &,
+                       const lpzrobots::OsgHandle &,
+                       lpzrobots::GlobalData &globalData,
+                       int key,
+                       bool down)
+  {
+    if (down)
+    { // only when key is pressed, not when released
+      switch (char(key))
+      {
+      case 'x':
+        if (robotfixator)
+        {
+          std::cout << "dropping robot" << std::endl;
+          delete robotfixator;
+          robotfixator = NULL;
+        }
+        break;
+      default:
+        return false;
+        break;
+      }
+    }
+    return false;
+  }
+
+protected:
+  lpzrobots::Joint *robotfixator;
+  AbstractController *controller;
+  lpzrobots::Hexabot *hexabot;
+};
+
+int main(int argc, char **argv)
+{
+  ThisSim sim;
+  sim.setGroundTexture("Images/greenground.rgb");
+  return sim.run(argc, argv) ? 0 : 1;
+}
